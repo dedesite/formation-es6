e3419320ddd1035ac2db721885d95fb69f660939
Andréas Livet <andreas.livet@gmail.com>
2017-12-14 12:26:39 +0100

    step 1 : package.json gulpfile and eslintrc

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+const gulp = require("gulp");
+const eslint = require("gulp-eslint");
+const jasmineBrowser = require("gulp-jasmine-browser");
+const watch = require("gulp-watch");
+
+gulp.task("lint", () => {
+  // ESLint ignores files with "node_modules" paths.
+  // So, it's best to have gulp ignore the directory as well.
+  // Also, Be sure to return the stream from the task;
+  // Otherwise, the task may end before the stream has finished.
+  return (
+    gulp
+      .src(["src/*.js"])
+      // eslint() attaches the lint output to the "eslint" property
+      // of the file object so it can be used by other modules.
+      .pipe(eslint())
+      // eslint.format() outputs the lint results to the console.
+      // Alternatively use eslint.formatEach() (see Docs).
+      .pipe(eslint.format())
+      // To have the process exit with an error code (1) on
+      // lint error, return the stream and pipe to failAfterError last.
+      .pipe(eslint.failAfterError())
+  );
+});
+
+gulp.task("default", ["lint"], () => {
+  // place code for your default task here
+});
+
+gulp.task("jasmine", function() {
+  const filesForTest = ["src/**/*.js", "spec/**/*-spec.js"];
+  return gulp
+    .src(filesForTest)
+    .pipe(watch(filesForTest))
+    .pipe(jasmineBrowser.specRunner())
+    .pipe(jasmineBrowser.server({ port: 8888 }));
+});
