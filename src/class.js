45a4c08a19c9368cfd3dfc93d48337356a31195b
Andréas Livet <andreas.livet@gmail.com>
2017-12-15 21:49:47 +0100

    step 3 : class solution

--- src/class.js
+++ src/class.js
@@ -1,60 +1,38 @@
-function Animal(name) {
-  this._name = name;
-}
+"use strict";
 
-// Propriété statique
-Animal.type = "Animal";
-Animal.getFamily = function() {
-  return "Animal";
-};
-
-Animal.prototype = {
-  talk: function() {
-    return "Hello";
-  }
-};
-// Pour plus d'info sur cette syntaxe voir :
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty
-Object.defineProperty(Animal.prototype, "name", {
-  get: function() {
-    return this._name;
-  },
-  set: function(name) {
+class Animal {
+  constructor(name) {
     this._name = name;
   }
-});
+  static getFamily() {
+    return "Animal";
+  }
+  talk() {
+    return "Hello";
+  }
+  get name() {
+    return this._name;
+  }
+  set name(name) {
+    this._name = name;
+  }
+}
+// Il n'est pas encore possible d'avoir des propriété statiques
+// http://exploringjs.com/es6/ch_classes.html#_static-data-properties
+Animal.type = "Animal";
 
-function Cat(name) {
-  Animal.call(this, name);
+class Cat extends Animal {
+  static getFamily() {
+    return "Felidae";
+  }
+  talk() {
+    return "Miaou";
+  }
 }
 Cat.type = "Cat";
-Cat.getFamily = function() {
-  return "Felidae";
-};
-// On utilise ici Object.create et pas Object.assign pour récupérer aussi
-// les propriétés non énumérables.
-// pour plus de détails voir
-// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Description
-Cat.prototype = Object.create(Animal.prototype);
-Cat.prototype.constructor = Cat;
-// On peut définir une méthode comme ça
-Cat.prototype.talk = function() {
-  return "Miaou";
-};
 
-function Dog(name) {
-  Animal.call(this, name);
-}
-// Il n'existe pas de méthodes simple pour hériter des propriétés statiques
-// d'une classe en ES5
-Dog.type = Animal.type;
-Dog.getFamily = Animal.getFamily;
-Dog.prototype = Object.create(Animal.prototype, {
-  // Ou comme ça.
-  talk: {
-    value: function() {
-      return "Waf waf";
-    }
+class Dog extends Animal {
+  talk() {
+    return "Waf waf";
   }
-});
-Dog.prototype.constructor = Dog;
+}
