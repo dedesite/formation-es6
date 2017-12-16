c87fd569661724b4542e296b36e4d48fd783ad45
Andréas Livet <andreas.livet@gmail.com>
2017-12-16 16:20:33 +0100

    step 4 : add array solution

--- src/array.js
+++ src/array.js
@@ -1,98 +1,47 @@
+"use strict";
+
 function findIndex(a, searchEl) {
-  var elIndex = -1;
-  for (var i = 0; i < a.length; i++) {
-    if (a[i] === searchEl) {
-      elIndex = i;
-      break;
-    }
-  }
-  return elIndex;
+  return a.findIndex(el => el === searchEl);
 }
 
 function find(a, searchEl) {
-  var findedEl = undefined;
-  for (var i = 0; i < a.length; i++) {
-    if (a[i] === searchEl) {
-      findedEl = a[i];
-      break;
-    }
-  }
-  return findedEl;
+  return a.find(el => el === searchEl);
 }
 
 function concat(a1, a2) {
-  var finalArr = [];
-  for (var i = 0; i < a1.length; i++) {
-    finalArr.push(a1[i]);
-  }
-  for (var i = 0; i < a2.length; i++) {
-    finalArr.push(a2[i]);
-  }
-  return finalArr;
+  return [...a1, ...a2];
 }
 
 function createFrom(a) {
-  var toCreate = [];
-  for (var i = 0; i < a.length; i++) {
-    toCreate.push(a[i]);
-  }
-  return toCreate;
+  return Array.from(a);
 }
 
 function initialize(defaultValue, times) {
-  var toInitialize = [];
-  for (var i = 0; i < times; i++) {
-    toInitialize.push(defaultValue);
-  }
-  return toInitialize;
+  return new Array(times).fill(defaultValue);
 }
 
 function forEach(a, func) {
-  for (var i = 0; i < a.length; i++) {
-    func(a[i], i);
+  for (const [ind, el] of a.entries()) {
+    func(el, ind);
   }
 }
 
 function filter(a, func) {
-  var filteredArr = [];
-  for (var i = 0; i < a.length; i++) {
-    if (func(a[i])) {
-      filteredArr.push(a[i]);
-    }
-  }
-  return filteredArr;
+  return a.filter(func);
 }
 
 function map(a, func) {
-  var mappedArr = [];
-  for (var i = 0; i < a.length; i++) {
-    mappedArr.push(func(a[i]));
-  }
-  return mappedArr;
+  return a.map(func);
 }
 
 function reduce(a, func, initialValue) {
-  var accumulator = initialValue;
-  for (var i = 0; i < a.length; i++) {
-    accumulator = func(accumulator, a[i]);
-  }
-  return accumulator;
+  return a.reduce(func, initialValue);
 }
 
 function some(a, func) {
-  for (var i = 0; i < a.length; i++) {
-    if (func(a[i])) {
-      return true;
-    }
-  }
-  return false;
+  return a.some(func);
 }
 
 function every(a, func) {
-  for (var i = 0; i < a.length; i++) {
-    if (!func(a[i])) {
-      return false;
-    }
-  }
-  return true;
+  return a.every(func);
 }
